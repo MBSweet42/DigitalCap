b819acc17103984d2b4a8ec8290564ec882e8faf
Ruth Wilken <cjwilken@gmail.com>
2026-08-29 13:04:10 -0400

    Phase 7D: prove platform resource discovery in RESPOND

---
+++ js/platform-resource-map.js
@@ -0,0 +1,9 @@
+// DigitalCap Platform Resource Mapping
+// Maps platform + purpose to resource ID
+// MVP: Roblox reporting only
+
+const PLATFORM_RESOURCE_MAPS = {
+  roblox: {
+    reporting: 'roblox-report-abuse'
+  }
+};
